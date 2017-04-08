2c8faba4de1cf98e0912b3f28e934e2f9dd5123e
Eric Kwan <ekwan@umass.edu>
2017-04-07 20:27:36 -0400

    fb8

--- app/components/feeditem.js
+++ app/components/feeditem.js
@@ -5,8 +5,6 @@ import Comment from './comment';
 import {postComment} from '../server'
 import {unlikeFeedItem} from '../server'
 import {likeFeedItem} from '../server'
-
-
 export default class FeedItem extends React.Component {
   constructor(props) {
     super(props);
@@ -23,78 +21,79 @@ export default class FeedItem extends React.Component {
   }
 
   /**
-  * Triggered when the user clicks on the 'like' or 'unlike' button.
-  */
- handleLikeClick(clickEvent) {
-   // Stop the event from propagating up the DOM tree, since we handle it here.
-   // Also prevents the link click from causing the page to scroll to the top.
-   clickEvent.preventDefault();
-   // 0 represents the 'main mouse button' -- typically a left click
-   // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
-   if (clickEvent.button === 0) {
-     // Callback function for both the like and unlike cases.
-     var callbackFunction = (updatedLikeCounter) => {
-       // setState will overwrite the 'likeCounter' field on the current
-       // state, and will keep the other fields in-tact.
-       // This is called a shallow merge:
-       // https://facebook.github.io/react/docs/component-api.html#setstate
-       this.setState({likeCounter: updatedLikeCounter});
-     };
+   * Triggered when the user clicks on the 'like' or 'unlike' button.
+   */
+  handleLikeClick(clickEvent) {
+    // Stop the event from propagating up the DOM tree, since we handle it here.
+    // Also prevents the link click from causing the page to scroll to the top.
+    clickEvent.preventDefault();
+    // 0 represents the 'main mouse button' -- typically a left click
+    // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+    if (clickEvent.button === 0) {
+      // Callback function for both the like and unlike cases.
+      var callbackFunction = (updatedLikeCounter) => {
+        // setState will overwrite the 'likeCounter' field on the current
+        // state, and will keep the other fields in-tact.
+        // This is called a shallow merge:
+        // https://facebook.github.io/react/docs/component-api.html#setstate
+        this.setState({likeCounter: updatedLikeCounter});
+      };
 
-     if (this.didUserLike()) {
-       // User clicked 'unlike' button.
-       unlikeFeedItem(this.state._id, 4, callbackFunction);
-     } else {
-       // User clicked 'like' button.
-       likeFeedItem(this.state._id, 4, callbackFunction);
-     }
-   }
- }
+      if (this.didUserLike()) {
+        // User clicked 'unlike' button.
+        unlikeFeedItem(this.state._id, 4, callbackFunction);
+      } else {
+        // User clicked 'like' button.
+        likeFeedItem(this.state._id, 4, callbackFunction);
+      }
+    }
+  }
 
- /**
-  * Returns 'true' if the user liked the item.
-  * Returns 'false' if the user has not liked the item.
-  */
- didUserLike() {
-   var likeCounter = this.state.likeCounter;
-   var liked = false;
-   // Look for a likeCounter entry with userId 4 -- which is the
-   // current user.
-   for (var i = 0; i < likeCounter.length; i++) {
-     if (likeCounter[i]._id === 4) {
-       liked = true;
-       break;
-     }
-   }
-   return liked;
- }
-
+  /**
+   * Returns 'true' if the user liked the item.
+   * Returns 'false' if the user has not liked the item.
+   */
+  didUserLike() {
+    var likeCounter = this.state.likeCounter;
+    var liked = false;
+    // Look for a likeCounter entry with userId 4 -- which is the
+    // current user.
+    for (var i = 0; i < likeCounter.length; i++) {
+      if (likeCounter[i]._id === 4) {
+        liked = true;
+        break;
+      }
+    }
+    return liked;
+  }
 
   render() {
     var data = this.state;
-    var contents;
     var likeButtonText = "Like";
     if (this.didUserLike()) {
       likeButtonText = "Unlike";
     }
+    var contents;
+
     switch(data.type) {
       case "statusUpdate":
-      // Create a StatusUpdate. Dynamically created React component: needs a key.
-      // Keys only need to be unique among *siblings*, so we can re-use the
-      // same key as the FeedItem.
+        // Create a StatusUpdate. Dynamically created React component: needs a key.
+        // Keys only need to be unique among *siblings*, so we can re-use the
+        // same key as the FeedItem.
         contents = (
           <StatusUpdate key={data._id} author={data.contents.author} postDate={data.contents.postDate} location={data.contents.location}>
             {data.contents.contents.split("\n").map((line, i) => {
+              // Note: 'i' is the index of line in data.contents.contents.
               return (
                 <p key={"line" + i}>{line}</p>
               );
             })}
           </StatusUpdate>
         );
-      break;
-    default:
-      throw new Error("Unknown FeedItem: " + data.type);
-  }
+        break;
+      default:
+        throw new Error("Unknown FeedItem: " + data.type);
+    }
 
     return (
       <div className="fb-status-update panel panel-default">
@@ -105,19 +104,15 @@ export default class FeedItem extends React.Component {
             <div className="col-md-12">
               <ul className="list-inline">
                 <li>
-                <a href="#" onClick={(e) => this.handleLikeClick(e)}>
-                  <span className="glyphicon glyphicon-thumbs-up">
-                  </span> {likeButtonText} </a>
+                  <a href="#" onClick={(e) => this.handleLikeClick(e)}>
+                    <span className="glyphicon glyphicon-thumbs-up"></span> {likeButtonText}
+                  </a>
                 </li>
                 <li>
-                <a href="#">
-                  <span className="glyphicon glyphicon-comment">
-                  </span> Comment</a>
+                <a href="#"><span className="glyphicon glyphicon-comment"></span> Comment</a>
                 </li>
                 <li>
-                <a href="#">
-                  <span className="glyphicon glyphicon-share-alt">
-                  </span> Share</a>
+                <a href="#"><span className="glyphicon glyphicon-share-alt"></span> Share</a>
                 </li>
               </ul>
             </div>
@@ -131,14 +126,16 @@ export default class FeedItem extends React.Component {
           </div>
           <hr />
           <CommentThread onPost={(commentText) => this.handleCommentPost(commentText)}>
-            {
-              data.comments.map((comment, i) => {
-                // i is comment's index in comments array
-                return (
-                  <Comment key={i} author={comment.author} postDate={comment.postDate}>{comment.contents}</Comment>
-                );
-              })
-            }
+          {
+            data.comments.map((comment, i) => {
+              // i is comment's index in comments array
+              return (
+                <Comment key={i} fId={this.state._id} author={comment.author}
+                  data={comment} postDate={comment.postDate}>{comment.contents}
+                </Comment>
+              );
+            })
+          }
           </CommentThread>
         </div>
       </div>
